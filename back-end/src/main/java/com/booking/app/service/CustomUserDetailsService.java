dfc2a4f469ab6ca9b6be43dcd7932bbbc88a5b81
Phong Vo <phongvo27ams@gmail.com>
2025-04-20 09:43:20 +0700

    Set up JWT for authentication and security

---
+++ back-end/src/main/java/com/booking/app/service/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.booking.app.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.booking.app.exception.MyException;
+import com.booking.app.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return userRepository.findByEmail(username).orElseThrow(() -> new MyException("Username/email not found."));
+  }
+}
