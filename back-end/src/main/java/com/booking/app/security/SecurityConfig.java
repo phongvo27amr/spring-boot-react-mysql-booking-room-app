d65cc01b954809ab302c14498820670f679f1b73
Phong Vo <phongvo27ams@gmail.com>
2025-04-23 23:16:53 +0700

    Fix authentication for booking to allow user or admin

--- back-end/src/main/java/com/booking/app/security/SecurityConfig.java
+++ back-end/src/main/java/com/booking/app/security/SecurityConfig.java
@@ -34,13 +34,14 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
-                .authorizeHttpRequests(request -> request
-                  .requestMatchers("/auth/**", "/rooms/**", "/bookings/**").permitAll()
-                  .anyRequest().authenticated())
-                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        .cors(Customizer.withDefaults())
+        .authorizeHttpRequests(request -> request
+            .requestMatchers("/auth/**", "/rooms/**").permitAll()
+            .requestMatchers("/bookings/**").hasAnyAuthority("USER", "ADMIN")
+            .anyRequest().authenticated())
+        .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authenticationProvider(authenticationProvider())
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
     return httpSecurity.build();
   }
@@ -60,7 +61,8 @@ public class SecurityConfig {
   }
 
   @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+      throws Exception {
     return authenticationConfiguration.getAuthenticationManager();
   }
 }
--- back-end/src/main/java/com/booking/app/utils/JwtUtils.java
+++ back-end/src/main/java/com/booking/app/utils/JwtUtils.java
@@ -1,5 +1,6 @@
 package com.booking.app.utils;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Date;
 import java.util.function.Function;
@@ -15,39 +16,39 @@ import io.jsonwebtoken.Jwts;
 
 @Service
 public class JwtUtils {
-  private static final long EXPIRATION_TIME = 1000 * 60 * 24 * 7; // 7 days
+    private static final long EXPIRATION_TIME = 1000 * 60 * 24 * 7; // 7 days
 
-  private final SecretKey Key;
+    private final SecretKey Key;
 
-  public JwtUtils() {
-    String base64Secret = "Gm5BfH6v9rXQ4KsH7KDUpiyMA6zrMwJXK6SRJ3MFLz0=";
-    byte[] keyBytes = Base64.getDecoder().decode(base64Secret);
-    this.Key = new SecretKeySpec(keyBytes, "HmacSHA256");
-  }
+    public JwtUtils() {
+        String secreteString = "843567893696976453275974432697R634976R738467TR678T34865R6834R8763T478378637664538745673865783678548735687R3";
+        byte[] keyBytes = Base64.getDecoder().decode(secreteString.getBytes(StandardCharsets.UTF_8));
+        this.Key = new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
 
-  public String generateToken(UserDetails userDetails) {
-    return Jwts.builder()
-        .subject(userDetails.getUsername())
-        .issuedAt(new Date(System.currentTimeMillis()))
-        .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-        .signWith(Key)
-        .compact();
-  }
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(Key)
+                .compact();
+    }
 
-  public String extractUsername(String token) {
-    return extractClaims(token, Claims::getSubject);
-  }
+    public String extractUsername(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
 
-  private <T> T extractClaims(String token, Function<Claims, T> claimsTFunction) {
-    return claimsTFunction.apply(Jwts.parser().verifyWith(Key).build().parseSignedClaims(token).getPayload());
-  }
+    private <T> T extractClaims(String token, Function<Claims, T> claimsTFunction) {
+        return claimsTFunction.apply(Jwts.parser().verifyWith(Key).build().parseSignedClaims(token).getPayload());
+    }
 
-  public boolean isValidToken(String token, UserDetails userDetails) {
-    final String username = extractUsername(token);
-    return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-  }
+    public boolean isValidToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
 
-  private boolean isTokenExpired(String token) {
-    return extractClaims(token, Claims::getExpiration).before(new Date());
-  }
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token, Claims::getExpiration).before(new Date());
+    }
 }
