01b37730db758457be8a63e1b3d5230f5b181aeb
Phong Vo <phongvo27ams@gmail.com>
2025-04-25 08:31:12 +0700

    Implement the Register and Login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,7 +6,9 @@ import AllRoomsPage from './component/booking_rooms/AllRoomsPage';
 import FindBookingPage from './component/booking_rooms/FindBookingPage';
 import Footer from './component/common/Footer';
 import Home from './component/home/Home';
+import LoginPage from './component/auth/LoginPage';
 import Navbar from './component/common/Navbar';
+import RegisterPage from './component/auth/RegisterPage';
 import RoomDetailsPage from './component/booking_rooms/RoomDetailsPage';
 
 import './App.css';
@@ -23,6 +25,8 @@ function App() {
             <Route exact path="/rooms" element={<AllRoomsPage />} />
             <Route path="/find-booking" element={<FindBookingPage />} />
             <Route path="/room-details-book/:roomId" element={<RoomDetailsPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
           </Routes>
         </div>
 
